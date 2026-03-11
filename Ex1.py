2b9a5af45e87553e29f5f73b0b4193f09c6185d8
joaopavan-ort <joao.pavan@ort.org.br>
2026-03-11 15:09:25 -0300

    Ajuste na questão 1

--- Ex1.py
+++ Ex1.py
@@ -11,4 +11,4 @@ Z = float(input('Escolha um número inteiro para a variável Z: '))
 
 print('O produto do triplo da variável X com o dobro da variável Y:', (3 * X) * (2 * Y))
 print('A soma de metade da variável X com a variável Z:', (X / 2) + Z)
-print('A variável Y elevada ao quadrado:', Y**1)
+print('A variável Y elevada ao quadrado:', Y**2)
